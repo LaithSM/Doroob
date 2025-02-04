315240ffca5f5aa3ca61995fb31f301823607ebd
LaithSM01 <laithsamhan1@gmail.com>
2025-02-04 17:39:27 +0300

    moved top nav content with mr-80

--- components/header/TopNav.vue
+++ components/header/TopNav.vue
@@ -1,10 +1,10 @@
 <template>
   <nav class="bg-[#1c3050] text-white">
     <div class="mx-auto lg:px-8">
-      <div class="flex justify-center items-center space-x-10 lg:px-20">
+      <div class="flex justify-center items-center space-x-10 lg:px-20 lg:mr-80">
         <div
           id="right_side_links"
-          class="hidden md:flex items-center space-x-6 space-x-reverse text-left"
+          class="hidden md:flex items-center space-x-6 space-x-reverse text-left whitespace-nowrap"
         >
           <a href="#" class="text-sm hover:text-green-600">الصفحة الرئيسية</a>
           <a href="#" class="text-sm hover:text-green-600">التدريب الإلكتروني</a>
@@ -16,11 +16,11 @@
 
         <div id="left_side_items" class="w-full md:w-auto flex">
           <div class="relative w-full md:w-auto">
-            <div id="content_container" class="relative flex h-11">
+            <div id="content_container" class="relative flex h-10">
               <button
                 class="flex-1 md:flex-initial text-white inline-flex items-center justify-center px-4 font-bold
                        bg-[#318720] hover:bg-[#45a049]
-                       md:bg-[#4CAF50] md:hover:bg-[#45a049] md:-skew-x-40"
+                       md:bg-[#4CAF50] md:hover:bg-[#45a049] md:-skew-x-40 md:pr-6"
               >
                <div class="flex md:skew-x-40">
                 <img class="h-4 w-4 ml-1 mt-1" src="../../assests/pic/person.png"/>
@@ -30,7 +30,7 @@
               <button
                 class="flex-1 md:flex-initial text-sm inline-flex items-center justify-center px-4
                        bg-[#1c3050] hover:bg-[#4CAF50]
-                       md:bg-[#33598b] md:hover:bg-[#4CAF50] md:-skew-x-40 font-bold"
+                       md:bg-[#23395f] md:hover:bg-[#4CAF50] md:-skew-x-40 font-bold"
               >
                 <div class="flex md:skew-x-40">
                 <img class="h-4 w-4 ml-1" src="../../assests/pic/suitCase.png" />
